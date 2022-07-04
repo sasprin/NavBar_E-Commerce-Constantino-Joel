fe845fc7be71a69d301047f7fd64002e9968c8a8
Joel Constantino <ignacion_constantino@outlook.es>
2022-07-04 19:58:09 -0300

    NavBar añadido

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+let styles = {
+    height: `200px`,
+    backgroundColor: `grey`,
+}
+
+
+
+
+const NavBar = () =>{
+    return <div>
+        <h1>E-Commerce</h1>
+        <button>Home</button>
+        <button>Tecnología</button>
+        <button>Automoviles</button>
+        <button>Libros</button>
+        <button>Login</button>
+    </div>
+
+};
+export default NavBar;
